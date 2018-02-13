e4ec6674da6ace36d5b556304ddf6bc677e54493
Vu Nguyen <vun@vision.local>
2018-02-13 15:15:52 +0200

    init Menu

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="container">
       <pp-home></pp-home>
+      <pp-menu></pp-menu>
     </div>
     <div class="container">
       <pp-footer></pp-footer>
@@ -16,12 +17,14 @@
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import Home from './components/Home.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   components: {
     ppHeader: Header,    // pp = PizzaPlanet, our components, not 3rd party components
     ppFooter: Footer,
-    ppHome: Home
+    ppHome: Home,
+    ppMenu: Menu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 col-md-6">
+      <table class="table table-hover">
+        <thead class="thead-default">
+          <tr>
+            <th>Size</th>
+            <th>Price</th>
+            <th>Add to basket</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Margherita</td>
+          </tr>
+          <tr>
+            <td>9"</td>
+            <td>12.95</td>
+            <td><button class="btn btn-sm btnoutline-success" type="button">+</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
