768b8495bbc137b220689dc784d193f44a898c32
Vu Nguyen <vun@vision.local>
2018-02-15 09:23:39 +0200

    add Login.vue to Admin.vue

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -52,15 +52,24 @@
       </table>
     </div>
   </div>
+
+  <hr>
+  <div class="row">
+    <div class="col-sm-12 col-lg-6">
+      <pp-login/>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 import NewPizza from './NewPizza.vue'
+import Login from './Login.vue'
 
 export default {
   components: {
-    ppNewPizza: NewPizza
+    ppNewPizza: NewPizza,
+    ppLogin: Login
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <form action="">
+      <div class="form-group">
+        <label>Email</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email"></input>
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter Password"></input>
+      </div>
+      <button class="btn btn-outline-success">Sign in</button>
+      <button class="btn btn-outline-danger">Sign out</button>
+    </form>
+  </div>
+</template>
