661f4f942406ac960123dbfe824abb3cae8820a5
Vu Nguyen <vun@vision.local>
2018-02-15 10:43:16 +0200

    add some example pages for vue-router

---
+++ src/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">Contact</h4>
+      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+      <p class="card-text">
+        <address>
+          <strong>Pizza Planet</strong><br>
+          Address line 1 <br>
+          Address line 2 <br>
+          Telephone: 4290542
+        </address>
+        <address>
+          <strong>Email:</strong><br>
+          <p>contact@pizzaplanet.com</p>
+        </address>
+      </p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+</template>
---
+++ src/components/History.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">History</h4>
+      <p class="card-text">
+        Some randome text
+      </p>
+    </div>
+  </div>
+</template>
