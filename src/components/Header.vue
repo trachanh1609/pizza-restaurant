f8920d73c446e69491bcdc0562693c5f6d30c1a0
Vu Nguyen <vun@vision.local>
2018-02-15 12:44:05 +0200

    improve router-link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,8 @@
     <nav class="navbar navbar-expand-lg navbar-light">
       <a href="/" class="navbar-brand">PIZZA PLANET</a>
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-link to="/"><a class="nav-link">Home</a></router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/menu"><a class="nav-link">Menu</a></router-link>
-        </li>
+          <router-link tag="li" to="/"><a class="nav-link">Home</a></router-link>
+          <router-link tag="li" to="/menu"><a class="nav-link">Menu</a></router-link>
       </ul>
     </nav>
   </header>
