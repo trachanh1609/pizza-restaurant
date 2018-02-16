68f7b38d5647205ab8a4dec71ba5cba112e3751e
Vu Nguyen <vun@vision.local>
2018-02-16 11:05:40 +0200

    routing using router methods

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,29 @@
       <div class="background">
         <h1>Welcome to Pizza Restaurant</h1>
         <h2>Feeling hungry ?</h2>
-        <button class="btn btn-success">Let's order !</button>
+        <button class="btn btn-success" @click="goToMenu">Let's order !</button>
       </div>
     </div>
   </div>
 </template>
+<script>
+
+  export default {
+    methods: {
+      goToMenu(){
+        // this.$router.go(-1)  // go back
+        // this.$router.replace('/menu')    // replace current, so go back may not work as expected
+        // this.$router.replace({name: 'menuLink'})  // we can pass in route name as well
+        this.$router.push({name: 'menuLink'})
+      }
+    }
+  }
+</script>
 <style>
   #home {
     background: url('../../src/assets/pizza.jpg');
     height: 800px;
-    padding-top: 150px; 
+    padding-top: 150px;
   }
   .background {
     padding: 20px 0;
