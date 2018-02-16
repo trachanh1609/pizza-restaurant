2509450dbb54a107bc28e3d283f4c07655cf02d7
Vu Nguyen <vun@vision.local>
2018-02-16 13:37:31 +0200

    refactor routes

--- src/main.js
+++ src/main.js
@@ -1,27 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from './components/Home.vue'
-import Menu from './components/Menu.vue'
-import About from './components/About.vue'
-import Contact from './components/Contact.vue'
-import History from './components/History.vue'
-import Delivery from './components/Delivery.vue'
-import Admin from './components/Admin.vue'
 import App from './App.vue'
-
+import {routes} from './routes'
 Vue.use(VueRouter);
 
-const routes = [
-  { path: '/', name: 'homeLink', component: Home},
-  { path: '/menu', name: 'menuLink',component: Menu},
-  { path: '/admin', name: 'adminLink',component: Admin},
-  { path: '/about', name: 'aboutLink', component: About, children: [
-    { path: 'contact', name: 'contactLink', component: Contact},
-    { path: 'history', component: History},
-    { path: 'delivery', component: Delivery},
-  ]},
-  { path: '*', redirect: '/'}
-]
 
 const router = new VueRouter({
   routes,  // short for routes: routes
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import Home from './components/Home.vue'
+import Menu from './components/Menu.vue'
+import About from './components/About.vue'
+import Contact from './components/Contact.vue'
+import History from './components/History.vue'
+import Delivery from './components/Delivery.vue'
+import Admin from './components/Admin.vue'
+
+export const routes = [
+  { path: '/', name: 'homeLink', component: Home},
+  { path: '/menu', name: 'menuLink',component: Menu},
+  { path: '/admin', name: 'adminLink',component: Admin},
+  { path: '/about', name: 'aboutLink', component: About, children: [
+    { path: 'contact', name: 'contactLink', component: Contact},
+    { path: 'history', component: History},
+    { path: 'delivery', component: Delivery},
+  ]},
+  { path: '*', redirect: '/'}
+]
