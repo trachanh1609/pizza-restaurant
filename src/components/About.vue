4d242ab224136d20cc119465badfc1efab10badf
Vu Nguyen <vun@vision.local>
2018-02-16 10:44:37 +0200

    update a link

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,14 @@
 <template>
 <div class="container">
-  <h1>About Us</h1>
+  <h2>About Us</h2>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a href="/" class="navbar-brand">PIZZA PLANET</a>
+    <ul class="navbar-nav">
+        <router-link tag="li" to="/about/history"><a class="nav-link">History</a></router-link>
+        <router-link tag="li" to="/about/contact"><a class="nav-link">Contact</a></router-link>
+        <router-link tag="li" to="/about/delivery"><a class="nav-link">Delivery</a></router-link>
+    </ul>
+  </nav>
   <router-view></router-view>
 </div>
 </template>
